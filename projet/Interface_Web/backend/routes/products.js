d9801baa30abbb7bc741e7d702a68427374a8f1b
Erwan GLASZIOU <wan.glasziou@gmail.com>
2021-01-27 11:12:19 +0100

    modèle fonctionnel

--- projet/Interface_Web/backend/routes/products.js
+++ projet/Interface_Web/backend/routes/products.js
@@ -3,19 +3,19 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/product');
 
-app.get('/api/products', (req, res, next) => {
+router.get('/api/products', (req, res, next) => {
   Product.find()
     .then(products => res.status(200).json({ products: Product }))
     .catch(error => res.status(400).json({ error }));
 });
 
-app.get('/api/products/:id', (req, res, next) => {
+router.get('/api/products/:id', (req, res, next) => {
   Product.findOne({ _id: req.params.id })
     .then(product => res.status(200).json({ product: Product }))
     .catch(error => res.status(404).json({ error }));
 });
 
-app.post('/api/products', (req, res, next) => {
+router.post('/api/products', (req, res, next) => {
   //delete req.body._id;
   const product = new Product({
     ...req.body
@@ -28,13 +28,13 @@ app.post('/api/products', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
-app.put('/api/products/:id', (req, res, next) => {
+router.put('/api/products/:id', (req, res, next) => {
   Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Modified!' }))
     .catch(error => res.status(400).json({ error }));
 });
 
-app.delete('/api/products/:id', (req, res, next) => {
+router.delete('/api/products/:id', (req, res, next) => {
   Product.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Deleted!' }))
     .catch(error => res.status(400).json({ error }));
