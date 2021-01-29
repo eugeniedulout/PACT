91b7fcfb74e03e856df96bf1acf05de2e435ae61
Erwan GLASZIOU <wan.glasziou@gmail.com>
2021-01-29 16:39:32 +0100

    Revert "petit backup entre 2 parties du tuto Fullstack. Token d'Authentification partiellement implémentés"
    This reverts commit f1077ed06fe32918acaaeff47d4d7b4b8f46b040.

--- projet/Interface_Web/backend/app.js
+++ projet/Interface_Web/backend/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 
--- projet/Interface_Web/backend/controllers/user.js
+++ projet/Interface_Web/backend/controllers/user.js
@@ -25,11 +25,7 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign(
-              { userId: user._id },
-              'RANDOM_TOKEN_SECRET',
-              { expiresIn: '24h' }
-            )
+            token: 'TOKEN'
           });
         })
         .catch(error => res.status(500).json({ error }));
