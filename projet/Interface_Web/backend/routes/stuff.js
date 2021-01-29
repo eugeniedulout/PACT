938673879cd3f7501f63f47f868099a93b8c1345
Erwan GLASZIOU <wan.glasziou@gmail.com>
2021-01-29 16:39:30 +0100

    Revert "Fin d'authentification"
    This reverts commit 82fb28b8b5412d6c9209062b77877ff2f6ac976b.

--- projet/Interface_Web/backend/routes/stuff.js
+++ projet/Interface_Web/backend/routes/stuff.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middleware/auth');
-
 const stuffCtrl = require('../controllers/stuff');
 
-router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', auth, stuffCtrl.createThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
-router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/', stuffCtrl.getAllStuff);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
 
 module.exports = router;
