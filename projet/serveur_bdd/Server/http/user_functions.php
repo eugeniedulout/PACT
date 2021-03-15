62ff52ec1fdc8aec182cddd576bc66233402bd0a
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-03-15 15:21:18 +0100

    Ajouts de commentaires

--- projet/serveur_bdd/Server/http/user_functions.php
+++ projet/serveur_bdd/Server/http/user_functions.php
@@ -1,8 +1,14 @@
 <?php
-
+//connexion à la base de donnée
 include("db_connect.php");
+
+//On vérifie qu'une action a bien été demandée
 if(isset($_POST['action'])) {
+
+	//On effectue le traitement selon l'action demandée
 	switch($_POST['action']) {
+
+	//obtenir le nom d'utilisateur	
 	case "get_username":
 		$response = $db->query("SELECT firstname, lastname FROM Users WHERE id=".$_POST['id']);
 		$data = $response->fetch();
