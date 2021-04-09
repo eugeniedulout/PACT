47f052b7cc69b54d81ab552279eb0586af6356f7
Erwan GLASZIOU <wan.glasziou@gmail.com>
2021-04-09 15:28:55 +0200

    màj du mdp

--- projet/Interface_Web/PHP/MAMP/htdocs/Login/cible.php
+++ projet/Interface_Web/PHP/MAMP/htdocs/Login/cible.php
@@ -1,6 +1,6 @@
 <?php session_start();
 try {
-	$db = new PDO('mysql:host=localhost;dbname=foodgps_db;charset=utf8','root','root'); //password will be poo4Zaec5e
+	$db = new PDO('mysql:host=localhost;dbname=foodgps_db;charset=utf8','root','poo4Zaec5e'); //password will be poo4Zaec5e
 }
 catch (Exception $e) {
 	die('Erreur : '. $e->getMessage());
--- projet/Interface_Web/PHP/MAMP/htdocs/Login/verification.php
+++ projet/Interface_Web/PHP/MAMP/htdocs/Login/verification.php
@@ -4,7 +4,7 @@
 
 <?php 
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=foodgps_db;charset=utf8','root','root');
+    $bdd = new PDO('mysql:host=localhost;dbname=foodgps_db;charset=utf8','root','poo4Zaec5e');
 }
   catch (Exception $e) {
     die('Erreur : '. $e->getMessage());
