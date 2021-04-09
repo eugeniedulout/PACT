ca0c110af72fd40591e66eee0b57143402f17ff4
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-04-09 13:44:33 +0200

    reponse en json

--- projet/serveur_bdd/Server/http/user_functions.php
+++ projet/serveur_bdd/Server/http/user_functions.php
@@ -1,25 +1,16 @@
 <?php
-//connexion à la base de donnée
 include("db_connect.php");
-
-//On vérifie qu'une action a bien été demandée
 if(isset($_POST['action'])) {
-
-	//On effectue le traitement selon l'action demandée
-	switch($_POST['action']) {
-
-	//obtenir le nom d'utilisateur	
-	case "get_username":
-		$response = $db->query("SELECT firstname, lastname FROM Users WHERE id=".$_POST['id']);
-		$data = $response->fetch();
-		if($data != null)
-			print("true%".$data['firstname']." ".$data['lastname']);
-		else
-			print("false");
-		break;
-	}
-
+        switch($_POST['action']) {
+        case "get_username":
+                $response = $db->query("SELECT firstname, lastname FROM Users WHERE id=".$_POST['id']);
+                $data = $response->fetch();
+                if($data != null) {
+                        $answer = ['ok' => true, 'username' => $data['username'] . " " . $data['lastname']];
+                }
+                $json_result = json_encode($answer);
+                print($json_result);
+                break;
+        }
 }
-
-
 ?>
