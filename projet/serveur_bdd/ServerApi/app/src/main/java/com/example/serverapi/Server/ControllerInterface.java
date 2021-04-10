1491f670a88e1ee8aa58cbfee81702ba01fb0efd
Quentin Audinet <quentin.audinet@telecom-paris.fr>
2021-04-10 14:26:46 +0200

    Mise à jour de l'interface avec les nouvelles fonctions'

--- projet/serveur_bdd/ServerApi/app/src/main/java/com/example/serverapi/Server/ControllerInterface.java
+++ projet/serveur_bdd/ServerApi/app/src/main/java/com/example/serverapi/Server/ControllerInterface.java
@@ -1,6 +1,9 @@
 package com.example.serverapi.Server;
 
+import com.example.serverapi.ExternClasses.ListProduct;
+import com.example.serverapi.ExternClasses.Market;
 import com.example.serverapi.ExternClasses.Product;
+import com.example.serverapi.ExternClasses.ProductOnSpecialOffer;
 
 import java.util.ArrayList;
 
@@ -14,4 +17,22 @@ public interface ControllerInterface {
 
     // Retourne la liste des produits d'un magasin particulier
     public  ArrayList<Product> getAllProducts(int marketId);
+
+    // Retourne l'ensemble des listes d'un utilisateur
+    public ArrayList<ListProduct> getUserLists(int userId);
+
+    // Ajoute une nouvelle liste à un utilisateur
+    public void addNewListOfProducts(int userId, ListProduct listProduct);
+
+    // Retourne l'ensemble des listes partagées entre l'amis friendId et l'utilisateur userId
+    public ArrayList<ListProduct> getFriendLists(int friendId, int userId);
+
+    // Retourne l'ensemble deds magasins présents dans la base de donnée
+    public ArrayList<Market> getAllMarkets();
+
+    // Retourne l'ensemble des promotions d'un magasin
+    public ArrayList<ProductOnSpecialOffer> getMarketOffers(int market_id);
+
+    // Retourne la liste des amis de user_id
+    public ArrayList<Integer> getUserFriends(int user_id);
 }
