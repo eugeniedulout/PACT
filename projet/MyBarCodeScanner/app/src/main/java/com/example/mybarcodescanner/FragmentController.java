3bb80adfad032889236deeacfa82ceb1082c5273
Zineb Senane <zinebsenane6@gmail.com>
2021-04-11 18:54:03 +0200

    push de la partie Scann de code barre intégrée dans la partie Android

---
+++ projet/MyBarCodeScanner/app/src/main/java/com/example/mybarcodescanner/FragmentController.java
@@ -0,0 +1,24 @@
+package com.example.mybarcodescanner;
+
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class FragmentController {
+
+
+
+    public static void swapFragment(Fragment dest, int containerId, Context context) {
+        FragmentTransaction fr = ((AppCompatActivity)context).getSupportFragmentManager().beginTransaction();
+        fr.replace(containerId,dest);
+        fr.addToBackStack(null).commit();
+    }
+
+    public static void swapFragmentInMainContainer(Fragment dest, Context context) {
+        swapFragment(dest, R.id.container, context);
+    }
+
+}
